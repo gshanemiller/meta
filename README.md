41036205032bd5eb86a93c4b13a34c5b3304048e
smiller <gshanemiller6@gmail.com>
2024-07-01 19:04:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# description
+
+*Input*: An array of strings city, and an array of unsigned integers population in 1:1 correspondence such that city[i] has population population[i]. Let T be the sum of entries in population. 
+API: A class accepting city, population as constructor const references equipped with a public method void print()
+
+*Output*: When print() is invoked it shoud print exactly one city indexed by i with probability probability city[i]/T.
+
+*Non-functional Performance*: print will be call millions of times. It should be performant.
+
+*Example*: city={NY,SF,LA}; population={20,10,30} e.g. LA popuation is 30. print() should write 'LA' to stdout 1/2th of the time since 30/T = 30/60 = 1/2.
+
+# build code
+./build
+
+# run code
+./run
+
+# sample output
+generating 10000 data points ...
+LA expected about 50 pct or about 5000
+5089
+NY expected about 33 pct or about 3333
+3239
+SF expected about 17 pct or about 1667
+1672
---
+++ meta.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <assert.h>
+#include <sys/types.h>
+
+class CityPop {
+  std::random_device                d_randDevice;
+  std::mt19937                      d_gen;
+  const std::vector<std::string>&   d_city;
+  std::uniform_real_distribution<>  d_dist;
+  std::vector<double>               d_weight;
+
+public:
+  CityPop() = delete;
+  CityPop(const CityPop& other) = delete;
+  CityPop& operator=(const CityPop& rhs) = delete;
+  ~CityPop() = default;
+  explicit CityPop(double total, const std::vector<std::string>& city, const std::vector<int32_t>& population);
+  void print();
+};
+
+CityPop::CityPop(double total, const std::vector<std::string>& city, const std::vector<int32_t>& population)
+: d_gen(d_randDevice())
+, d_city(city)
+{
+  assert(d_city.size()>1);
+  assert(d_city.size()==population.size());
+  d_weight.resize(population.size());
+  d_weight[0]=population[0]/total;
+  for (u_int64_t i=1; i<population.size(); ++i) {
+    d_weight[i]=d_weight[i-1]+population[i]/total;
+  }
+}
+
+void CityPop::print() {
+  const double sample = d_dist(d_gen);
+  for (u_int64_t i=0; i<d_weight.size(); ++i) {
+    if (sample<=d_weight[i]) {
+      printf("%s\n", d_city[i].c_str());
+      return;
+    }
+  }
+  printf("%s\n", d_city[d_city.size()-1].c_str());
+}
+
+int main() {
+  std::vector<std::string> city = {"LA", "NY", "SF"};
+  std::vector<int32_t>     pop  = {30, 20, 10};
+  CityPop obj(60.0, city, pop);
+  for (u_int64_t i=0; i<10000; ++i) {
+    obj.print();
+  }
+  return 0;
+}
